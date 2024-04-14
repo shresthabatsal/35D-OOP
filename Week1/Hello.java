28d85ae079460fd1d1a095e0ac3576d5e3ad970d
batsal <shresthabatsal247@gmail.com>
2024-04-14 12:24:21 +0545

    Week 1 task 2

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -4,5 +4,12 @@ public class Hello{
     public static void main(String[] args) {
         /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello World!");
+        /*2. Extend/Modify the above program to print two additional lines:
+
+This is my first program
+
+I am on module ST4003CEM */
+        System.out.print("This is my first program");
+        System.out.print("\nI am on module ST4003CEM");
     }
 }
